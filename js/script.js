4f0a72c596e905a09ba670805c362a1b14b19914
The Most Awesome of Awesome <KatherineOriez@mwc-146126.dhcp.missouri.edu>
2015-02-02 14:24:33 -0600

    BaseballStatTest

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+    getdata();
 });
 
-
-
-
-
-
+function getdata () {
+  $.getJSON("js/JayBruceStats.json", function(data, error) {
+  	playerData=data;
+  	drawStuff();
+  });
+ }
+ 
+function drawStuff(){
+	$(".chart").html(playerData.name);
+	
+	$.each(playerData.stats, function(i, item) {
+		$(".chart").append("<p>"+item.AVG+"</p>");
+	});
+	
+}
